8ed310e465f091c16ea392fd659b48fe2ce70390
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-11-29 13:00:53 +0300

    nothing serious

--- Week_2_PacMan/PacMan/pacman.py
+++ Week_2_PacMan/PacMan/pacman.py
@@ -9,4 +9,47 @@ class PacMan(GameObject):
 
 
 def compose_context(screen):
-    return {}
+    return {
+        "pacman": PacMan(screen.get_width() // 2, screen.get_height() // 2),
+    }
+
+
+def draw_whole_screen(screen, context):
+    screen.fill("gray")
+    context["pacman"].draw(screen)
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((1280, 960))
+    clock = pygame.time.Clock()
+    running = True
+    player_speed = 3
+    dt = 0
+
+    context = compose_context(screen)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        draw_whole_screen(screen, context)
+        pygame.display.flip()
+
+        keys = pygame.key.get_pressed()
+        old_player_topleft = context["pacman"].rect.topleft
+        if keys[pygame.K_w]:
+            context["pacman"].rect = context["pacman"].rect.move(0, -1 * player_speed)
+        if keys[pygame.K_s]:
+            context["pacman"].rect = context["pacman"].rect.move(0, player_speed)
+        if keys[pygame.K_a]:
+            context["pacman"].rect = context["pacman"].rect.move(-1 * player_speed, 0)
+        if keys[pygame.K_d]:
+            context["pacman"].rect = context["pacman"].rect.move(player_speed, 0)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
